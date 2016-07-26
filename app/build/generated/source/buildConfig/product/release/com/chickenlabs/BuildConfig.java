51c8bbbd985efd213d490a5526066c1055f1fc87
chyccs <chyccs@naver.com>
2016-07-26 14:12:10 +0900

    init

---
+++ app/build/generated/source/buildConfig/product/release/com/chickenlabs/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.chickenlabs;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.chickenlabs";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "product";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "1.0.3";
+}
